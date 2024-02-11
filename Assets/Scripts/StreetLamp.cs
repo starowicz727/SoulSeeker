b14b312c0760da59a78afe1c60d18a5771759e4d
starowicz727 <starowicz727@gmail.com>
2024-02-11 23:02:40 +0100

    street lights

---
+++ Assets/Scripts/StreetLamp.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StreetLamp : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] Light lampLight;
+
+    private bool switchOff = false;
+    private float speed = 3f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            switchOff = false;
+            SwitchON();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            switchOff= true;
+        }
+    }
+
+    private void Update()
+    {
+        if(switchOff)
+        {
+            SwitchOFF();
+        }
+    }
+
+    void SwitchON()
+    {
+        if (lampLight.intensity < 4)
+        {
+            lampLight.intensity += speed * Time.deltaTime;
+        }
+    }
+    void SwitchOFF()
+    {
+        if (lampLight.intensity > 0)
+        {
+            lampLight.intensity -= speed * Time.deltaTime;
+            Debug.Log(lampLight.intensity);
+        }
+    }
+}
